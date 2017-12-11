5fa897c5ae3dd0551dcd2d2f8aafe51569661fda
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2017-12-11 17:47:44 -0500

    Add files via upload

--- Final.R
+++ Final.R
@@ -19,36 +19,46 @@ mbti <- mbti %>%
 mbti <- mbti[!(is.na(mbti$posts) | mbti$posts==""), ]
 mbti$isURL <- grepl('$http',mbti$posts,TRUE)
 mbti <- subset(mbti,mbti$isURL == 0)
+mbti$type <- strtrim(mbti$type,1)
 
-mbtiQ <- corpus(mbti,text_field = "posts")
-mbtiDF <- tidy(mbtiQ)
-mbtiDF$ntok <- ntoken(mbtiDF$text)
-mbtiDF$ntyp <- ntype(mbtiDF$text)
-mbtiDF$uniq <- 100.0 / (mbtiDF$ntok/mbtiDF$ntyp)
-mbtiDF$avgWPS <- mbtiDF$ntok / nsentence(mbtiDF$text)
-mbtiDF <- tibble::rowid_to_column(mbti, "ID")
+entQ <- corpus(subset(mbti,mbti$type=="E"),text_field = "posts")
+intQ <- corpus(subset(mbti,mbti$type=="I"),text_field = "posts")
+entDF <- tidy(entQ)
+intDF <- tidy(intQ)
+
+entDF$ntok <- ntoken(entDF$text)
+entDF$ntyp <- ntype(entDF$text)
+entDF$uniq <- 100.0 / (entDF$ntok/entDF$ntyp)
+entDF$avgWPS <- entDF$ntok / nsentence(entDF$text)
+entDF <- tibble::rowid_to_column(entDF, "ID")
+
+intDF$ntok <- ntoken(intDF$text)
+intDF$ntyp <- ntype(intDF$text)
+intDF$uniq <- 100.0 / (intDF$ntok/intDF$ntyp)
+intDF$avgWPS <- intDF$ntok / nsentence(intDF$text)
+intDF <- tibble::rowid_to_column(intDF, "ID")
 
 #sentiment analysis
-mbtiS <- dfm(mbtiQ, dictionary = data_dictionary_LSD2015)
-mbtiS <- tidy(mbtiS)
-mbtiPlot <- cbind(mbtiS) # make a copy to use later
-mbtiS <- mutate(mbtiS, ID = as.numeric(rownames(mbtiS)))
-mbtiN <- subset(mbtiS,mbtiS$term=="negative")
-mbtiN$document <- as.integer(mbtiN$document)
-mbtiDF <- inner_join(mbtiDF,mbtiN,by = c("ID" = "document"))[c("ID","type","posts","count","uniq","avgWPS")]
-names(mbtiDF)[names(mbtiDF) == 'count'] <- 'negative'
-mbtiP <- subset(mbtiS,mbtiS$term=="positive")
-mbtiP$document <- as.integer(mbtiP$document)
-mbtiDF <- inner_join(mbtiDF,mbtiP,by = c("ID" = "document"))[c("ID","type","posts","negative","count","uniq","avgWPS")]
-names(mbtiDF)[names(mbtiDF) == 'count'] <- 'positive'
-mbtiDF <- data.frame(mbtiDF)
+entS <- dfm(entQ, dictionary = data_dictionary_LSD2015)
+entS <- tidy(entS)
+entPlot <- cbind(entS) # make a copy to use later
+entS <- mutate(entS, ID = as.numeric(rownames(entS)))
+entN <- subset(entS,entS$term=="negative")
+entN$document <- as.integer(entN$document)
+
+intS <- dfm(intQ, dictionary = data_dictionary_LSD2015)
+intS <- tidy(intS)
+intPlot <- cbind(intS) # make a copy to use later
+intS <- mutate(intS, ID = as.numeric(rownames(intS)))
+intN <- subset(intS,intS$term=="negative")
+intN$document <- as.integer(intN$document)
 
 link2 <- getURL("https://raw.githubusercontent.com/SamCD/Data607Final/master/Starbucks.csv")
 samples <- read.csv(text = link2,header = FALSE)
 key <- 'AIzaSyAq8j_r8PZJE2rtNTGjE4HbfMZbm7Njbxc'
 resDF <- data.frame(rating = as.numeric(character()),text = character())
 samples <- samples[sample(nrow(samples)),]
-for (row in 1:nrow(head(samples),10)) {
+for (row in 1:10) {
   lat <- samples[row,2]
   lon <- samples[row,1]
   res <- google_places(location = c(lat, lon),
@@ -70,51 +80,45 @@ resDF$avgWPS <- resDF$ntok / nsentence(resDF$text)
 resDF <- tibble::rowid_to_column(resDF, "ID")
 #sentiment analysis
 resS <- dfm(resQ, dictionary = data_dictionary_LSD2015)
-resS <- tidy(resS)
+#resS <- tidy(resS)
 resPlot <- cbind(resS) #make a copy to use later
-resS <- mutate(resS, ID = as.numeric(rownames(resS)))
-resN <- subset(resS,resS$term=="negative")
-resN$document <- as.integer(resN$document)
-resDF <- inner_join(resDF,resN,by = c("ID" = "document"))[c("ID","rating","text","count")]
-names(resDF)[names(resDF) == 'count'] <- 'negative'
-resP <- subset(resS,resS$term=="positive")
-resP$document <- as.integer(resP$document)
-resDF <- inner_join(resDF,resP,by = c("ID" = "document"))[c("ID","rating","text","negative","count")]
-names(resDF)[names(resDF) == 'count'] <- 'positive'
 resDF <- data.frame(resDF)
 
-count(mbtiDF,"type")
-mbtiDF$isINFP <- (mbti$type=="INFP")
+#mbtiDF <- data.frame(rbind(entDF,intDF))
+#set.seed(1234)
+#ind <- sample(2, nrow(mbtiDF), replace=TRUE, prob=c(0.67, 0.33))
+#mbtiDF$ind <- ind
+#tested <- subset(mbtiDF$ind,mbtiDF$ind!=1)
+#model= glm(type ~ uniq+avgWPS, mbtiDF)
+#p = predict(model, mbtiDF)
+#plot(p - tested)
 
-set.seed(1234)
-ind <- sample(2, nrow(mbtiDF), replace=TRUE, prob=c(0.67, 0.33))
-mbtiDF$ind <- ind
-tested <- mbti$ind
-model= lm(isINFP ~ uniq+avgWPS+negative+positive, subset(mbtiDF,mbtiDF$ind==1),)
-p = predict(model, subset(mbtiDF,mbtiDF$ind!=1))
-plot(p - tested)
-model=rpart(isINFP ~ uniq+avgWPS+negative+positive, subset(mbtiDF,mbtiDF$ind==1),)
-p = predict(model, subset(mbtiDF,mbtiDF$ind!=1))
-plot(p - tested)
-#fit <- glm(isINFP~uniq+avgWPS+negative+positive,data=mbtiDF,family=binomial())
-predict(model, resDF)
+pie(table(entPlot$term))
+pie(table(intPlot$term))
 
-mbtiPlotted = ggplot(mbtiPlot, mapping = aes(x = type, fill = term)) +
-  geom_bar(stat='count', position='fill') +
-  labs(x = 'MBTI Type') +
-  scale_fill_discrete(name="Word Sentiment") +
-  theme_few()
+#mbtiPlotted <- ggplot(mbtiPlot, aes(x = term, fill  = )) +
+#  geom_bar(position = "fill") +
+#  labs(x = 'MBTI Type') +
+#  scale_fill_discrete(name="Word Sentiment")
 
-resPlotted = ggplot(resPlot, mapping = aes(x = rating, fill = term)) +
-  geom_bar(stat='count', position='fill') +
-  labs(x = 'Rating Score') +
-  scale_fill_discrete(name="Word Sentiment") +
-  theme_few()
+#resPlotted = ggplot(resPlot, mapping = aes(x = rating, fill = term)) +
+#  geom_bar(stat='count', position='fill', inherit.aes = FALSE ) +
+#  labs(x = 'Rating Score') +
+#  scale_fill_discrete(name="Word Sentiment")
 
-multiplot(mbtiPlotted,resPlotted)
+entWC <- dfm(entQ, remove = stopwords("english"), remove_punct = TRUE)
+intWC <- dfm(intQ, remove = stopwords("english"), remove_punct = TRUE)
 
-#mbtiWC <- dfm(mbtiQ, remove = stopwords("english"), remove_punct = TRUE)
-#set.seed(100)
-#textplot_wordcloud(mbtiWC, min.freq = 6, random.order = FALSE,
-#                   rot.per = .25, 
-#                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+set.seed(100)
+textplot_wordcloud(entWC, min.freq = 6, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"),
+                   scale=c(4,.5),
+                   max.words =40)
+textplot_wordcloud(entWC, min.freq = 6, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"),
+                   scale=c(4,.5),
+                   max.words =40)
+
+
